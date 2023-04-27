e970762da8b9949bd60938265bc3d25d07d03de8
Mateo Vasco <macv9620@utp.edu.co>
2023-04-27 00:35:22 -0500

    Se implementan ventanas de validación al eliminar ítems

--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 
-const SECURITY_CODE = 'paradigma'
+const SECURITY_CODE = "paradigma";
 
 function UseState({ name }) {
-console.log('INICIO APP');
-  const[state, setState] = useState({
-    inputValue: '',
+  console.log("INICIO APP");
+  const [state, setState] = useState({
+    inputValue: "",
     error: false,
     loading: false,
-  })
+    deleted: false,
+    confirmed: false,
+  });
 
   // const [inputValue, setInputValue] = React.useState('');
   // const [error, setError] = React.useState(false);
@@ -16,30 +18,30 @@ console.log('INICIO APP');
 
   console.log("Empezando efecto");
   React.useEffect(() => {
-    const requestToBack = ()=>{
-
-      if(state.inputValue===SECURITY_CODE){
-        console.log('Contraseña correcta');
+    const requestToBack = () => {
+      if (state.inputValue === SECURITY_CODE) {
+        console.log("Contraseña correcta");
         setState({
           ...state,
           error: false,
           loading: false,
-        })
+          confirmed: true,
+        });
       } else {
-        console.log('ERROR');
+        console.log("ERROR");
         setState({
           ...state,
           error: true,
           loading: false,
-        })
+        });
         console.log(state);
         //setError(true)
       }
 
-        console.log(state);
-        //setLoading(false);
-        console.log("Finalizando consulta al BackEnd");
-      }
+      console.log(state);
+      //setLoading(false);
+      console.log("Finalizando consulta al BackEnd");
+    };
     if (state.loading) {
       console.log("Iniciando consulta al BackEnd");
 
@@ -49,32 +51,84 @@ console.log('INICIO APP');
 
   console.log("Terminando efecto");
 
-  const catchValue = (event)=>{
+  const catchValue = (event) => {
     setState({
       ...state,
       inputValue: event.target.value,
-    })
+    });
     //setInputValue(event.target.value)
-  }
+  };
 
-  const executeCompareBackPass = ()=>{
+  const executeCompareBackPass = () => {
     setState({
       ...state,
       error: false,
       loading: true,
-    })
-  }
+    });
+  };
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>Por favor escribe el código de seguridad</p>
-      {state.error && <p>Error: el código es incorrecto</p>}
-      {state.loading && <p>Loading...</p>}
-      <input placeholder="Código de seguridad" value={state.inputValue} onChange={catchValue}/>
-      <button onClick={executeCompareBackPass}>Comprobar</button>
-    </div>
-  );
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>Por favor escribe el código de seguridad</p>
+        {state.error && <p>Error: el código es incorrecto</p>}
+        {state.loading && <p>Loading...</p>}
+        <input
+          placeholder="Código de seguridad"
+          value={state.inputValue}
+          onChange={catchValue}
+        />
+        <button onClick={executeCompareBackPass}>Comprobar</button>
+      </div>
+    );
+  } else if (state.confirmed && !state.deleted) {
+    return (
+      <>
+        <h2>Confirmar eliminación</h2>
+        <p>¿Seguro que deseas eliminar la información?</p>
+        <div>
+          <button
+            onClick={()=>{
+              setState({
+                ...state,
+                deleted: true,
+              });
+            }}
+          >Sí, eliminar</button>
+          <button
+            onClick={() => {
+              setState({
+                ...state,
+                confirmed: false,
+              });
+            }}
+          >
+            No, volver
+          </button>
+        </div>
+      </>
+    );
+  } else {
+    //si ambos son true
+    return (
+      <>
+        <h2>Ítem eliminado</h2>
+        <button
+            onClick={() => {
+              setState({
+                ...state,
+                confirmed: false,
+                deleted: false,
+                inputValue: '',
+              });
+            }}
+          >
+            Volver a inicio
+          </button>
+      </>
+    );
+  }
 }
 
 export { UseState };
